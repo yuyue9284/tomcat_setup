26958cb91cf448d6d2b511bba09f49b221033869
Yue Yu <yy2636@columbia.edu>
2016-03-13 19:03:04 -0400

    add files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # tomcat_setup
+Remember set the path in the ~/.bashrc
+$export CATALINA_HOME=~/apache-tomcat-8.0.32
+$export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+$. ~/.bashrc
+
+Start the service
+$cd ~
+$sudo ./apache-tomcat-8.0.32/bin/startup.sh
+
+Stop the service 
+$sudo ./apache-tomcat-8.0.32/bin/shutdown.sh
+
---
+++ install_tomcat.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd ~
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+echo 'Download Tomcat'
+wget http://mirror.symnds.com/software/Apache/tomcat/tomcat-8/v8.0.32/bin/apache-tomcat-8.0.32.tar.gz
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+echo 'Unzip'
+tar xzf apache-tomcat-8.0.32.tar.gz
+rm apache-tomcat-8.0.32.tar.gz
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+echo 'Change Permission'
+sudo chmod +x ~/apache-tomcat-8.0.32/bin/*
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+echo 'Install jdk'
+sudo apt-get install openjdk-7-jdk
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+echo 'Set Path'
+export CATALINA_HOME=~/apache-tomcat-8.0.32
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+echo 'Config Test'
+sudo ./apache-tomcat-8.0.32/bin/configtest.sh
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+echo 'Start Service'
+sudo ./apache-tomcat-8.0.32/bin/startup.sh
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+
+echo 'Add these to ~/.bashrc'
+echo 'export CATALINA_HOME=~/apache-tomcat-8.0.32'
+echo 'export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64'
